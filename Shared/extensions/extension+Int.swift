d9a1109c2b2cab6ce485ffc52afe67d06ff7d76d
sirily11 <sirily1997@gmail.com>
2021-02-27 17:29:00 +0800

    fix time format

--- Shared/extensions/extension+Int.swift
+++ Shared/extensions/extension+Int.swift
@@ -8,7 +8,20 @@
 import Foundation
 extension Int{
     func secondsToHoursMinutesSeconds() -> (String, String, String) {
-      return (String(format: "%02d", self / 3600), String(format: "%02d", (self % 3600) / 60), String(format: "%02d",  (self % 3600) % 60))
+        let hr =  self / 3600
+        let min = (self % 3600) / 60
+        let sec = (self % 3600) % 60
+        
+        var hrs = String(format: "%02d", abs(hr))
+        let mins = String(format: "%02d", abs(min))
+        let secs =  String(format: "%02d",  abs(sec))
+        
+        
+        if hr < 0 || min < 0 || sec < 0{
+            hrs = "-\(hrs)"
+        }
+        
+      return (hrs,mins,secs)
     }
 
 }
